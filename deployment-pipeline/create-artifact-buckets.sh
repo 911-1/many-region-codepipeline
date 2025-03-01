97e858ad541c45f3e212ac36cc313cec93f2d32c
Rory <techhelpu8@gmail.com>
2025-03-01 12:17:41 -0500

    just make a ticket ;p

--- deployment-pipeline/create-artifact-buckets.sh
+++ deployment-pipeline/create-artifact-buckets.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+# *** *Make ticket* Logic is required to only deploy items to regions in which the api endpoints reside. As an example, if managed AD doesn't exist in the region, buckets shouldn't be built, haulting deployment.
+
 # *** Change this region list to the regions you want to deploy your application to
 REGIONS="us-east-1 us-west-2"
 
